fcea305fb53e8200a7badce67862bc1b4b0b9bc3
eyal-hl <eyalhermonilahat@gmail.com>
2024-01-13 01:01:07 +0200

    no longer groups all players in the same team

--- Back/utils.py
+++ Back/utils.py
@@ -24,18 +24,22 @@ def format_number_ranges(numbers):
 
 # takes a list of objects that have team_id and year, and returns it grouped by team_id and with years property
 # that is all the years formatted to be more readable
-def format_team_years(player_data):
+def format_team_years(player_data, ascend=False):
     formatted_teams = {}
     for playing_year in player_data:
-        if playing_year['team_id'] not in formatted_teams.keys():
-            formatted_teams[playing_year['team_id']] = {'base': playing_year, 'years': [playing_year['year']],
-                                                        'players': []}
+        key = playing_year['team_id'] + (str(playing_year['player_id']) if 'player_id' in playing_year.keys() else '')
+        if key not in formatted_teams.keys():
+            formatted_teams[key] = {'base': playing_year,
+                                                                                    'years': [playing_year['year']],
+                                                                                    'players': []}
         else:
-            formatted_teams[playing_year['team_id']]['years'].append(playing_year['year'])
+            formatted_teams[key]['years'].append(playing_year['year'])
 
     result = []
     for team in formatted_teams.values():
         team['base']['years'] = format_number_ranges(team['years'])
         result.append(team['base'])
+    if ascend:
+        result.reverse()
 
     return result
