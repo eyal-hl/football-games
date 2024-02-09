ae22366b2776819b40077dc7ca9b1caf1e56003a
eyal-hl <eyalhermonilahat@gmail.com>
2024-02-09 22:18:57 +0200

    Update origins and workers configuration

--- Back/main.py
+++ Back/main.py
@@ -25,7 +25,8 @@ app = FastAPI(
 )
 
 origins = [
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
 ]
 
 app.add_middleware(
--- Back/runServer.bat
+++ Back/runServer.bat
@@ -1,1 +1,1 @@
-.\.venv\Scripts\uvicorn.exe main:app --host 0.0.0.0 --port 80 --workers 4
+.\.venv\Scripts\uvicorn.exe main:app --host 0.0.0.0 --port 80 --workers 1
