f70d4194d7f9621e912bf80f993da204b1d27c4f
eyal-hl <eyalhermonilahat@gmail.com>
2024-02-13 22:52:53 +0200

    Fix formatting issues in Frontend code

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -6,34 +6,34 @@ import GridGame from './Components/GridGame/GridGame';
 import { ThemeProvider, createTheme } from '@mui/material';
 
 function App() {
-	const theme = createTheme({
-		components: {
-			MuiTooltip: {
-				styleOverrides: {
-					tooltip: {
-						fontSize: '1.2rem'
-					}
-				}
-			}
-		},
-		typography: {
-			fontFamily: 'Suii'
-		}
-	})
+  const theme = createTheme({
+    components: {
+      MuiTooltip: {
+        styleOverrides: {
+          tooltip: {
+            fontSize: '1.2rem',
+          },
+        },
+      },
+    },
+    typography: {
+      fontFamily: 'Suii',
+    },
+  });
 
-	return (
-		<div className='App'>
-			<ThemeProvider theme={theme}>
-				<BrowserRouter>
-					<Routes>
-						<Route path='/' element={<Home />} />
-						<Route path='/about' element={<About />} />
-						<Route path='/grid' element={<GridGame />} />
-					</Routes>
-				</BrowserRouter>
-			</ThemeProvider>
-		</div>
-	);
+  return (
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/grid" element={<GridGame />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
--- Front/src/Components/GridGame/FiltersSelectorDropDown/FiltersSelectorDropDown.tsx
+++ Front/src/Components/GridGame/FiltersSelectorDropDown/FiltersSelectorDropDown.tsx
@@ -7,38 +7,40 @@ import { translateFilters } from '../../../utils/utils';
 import { defaultPresets } from '../../../utils/consts';
 
 interface FiltersSelectorDropDownProps {
-	onChange: (filters: Filter[]) => void;
+  onChange: (filters: Filter[]) => void;
 }
 
 const FiltersSelectorDropDown = ({ onChange }: FiltersSelectorDropDownProps) => {
-	const [selectedFilters, setSelectedFilters] = useState<string>('default');
-	const localStoragePresets = localStorage.getItem('presets');
-	const presets: { [key: string]: filterSchema[] } = localStoragePresets ? JSON.parse(localStoragePresets) : defaultPresets;
-	const handlePresetChange = (preset: string) => {
-		setSelectedFilters(preset);
-		onChange(translateFilters(presets[preset]));
-	};
+  const [selectedFilters, setSelectedFilters] = useState<string>('default');
+  const localStoragePresets = localStorage.getItem('presets');
+  const presets: { [key: string]: filterSchema[] } = localStoragePresets
+    ? JSON.parse(localStoragePresets)
+    : defaultPresets;
+  const handlePresetChange = (preset: string) => {
+    setSelectedFilters(preset);
+    onChange(translateFilters(presets[preset]));
+  };
 
-	return (
-		<FormControl className={css.filterSelectorDropDown}>
-			<InputLabel id='demo-simple-select-label'>Filters</InputLabel>
-			<Select
-				className={css.select}
-				labelId='demo-simple-select-label'
-				id='demo-simple-select'
-				value={selectedFilters}
-				label='Filters'
-				defaultValue={'default'}
-				onChange={e => handlePresetChange(e.target.value as string)}
-			>
-				{Object.keys(presets).map(preset => (
-					<MenuItem key={preset} value={preset}>
-						{preset}
-					</MenuItem>
-				))}
-			</Select>
-		</FormControl>
-	);
+  return (
+    <FormControl className={css.filterSelectorDropDown}>
+      <InputLabel id="demo-simple-select-label">Filters</InputLabel>
+      <Select
+        className={css.select}
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={selectedFilters}
+        label="Filters"
+        defaultValue={'default'}
+        onChange={(e) => handlePresetChange(e.target.value as string)}
+      >
+        {Object.keys(presets).map((preset) => (
+          <MenuItem key={preset} value={preset}>
+            {preset}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
 };
 
 export default FiltersSelectorDropDown;
