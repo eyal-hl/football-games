7ab8ef54298fae8b93ac49fc146d2f594ce7fa82
eyal-hl <eyalhermonilahat@gmail.com>
2025-01-04 12:08:27 +0200

    redesigned home page

--- Front/src/Components/Home/Home.tsx
+++ Front/src/Components/Home/Home.tsx
@@ -1,12 +1,35 @@
-import SearchBox from '../SearchBox/SearchBox';
+import { useNavigate } from 'react-router-dom';
 import css from './Home.module.css';
+import GridViewIcon from '@mui/icons-material/GridView';
+import AccountTreeIcon from '@mui/icons-material/AccountTree';
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={css.home}>
-      <h1>Home</h1>
-      <p>This is the home page.</p>
-      <SearchBox onSelect={(player) => console.log(player)} />
+      <div className={css.hero}>
+        <div className={css.heroTitle}>Football Explorer</div>
+        <div className={css.heroSubtitle}>Discover football in different ways</div>
+      </div>
+
+      <div className={css.games}>
+        <div className={css.gameCard} onClick={() => navigate('/grid')}>
+          <div className={css.iconWrapper}>
+            <GridViewIcon className={css.icon} />
+          </div>
+          <div className={css.gameTitle}>Grid Game</div>
+          <div className={css.gameDescription}>Find players matching multiple criteria in a grid-based challenge</div>
+        </div>
+
+        <div className={css.gameCard} onClick={() => navigate('/connections')}>
+          <div className={css.iconWrapper}>
+            <AccountTreeIcon className={css.icon} />
+          </div>
+          <div className={css.gameTitle}>Connections</div>
+          <div className={css.gameDescription}>Explore how players are connected through shared team history</div>
+        </div>
+      </div>
     </div>
   );
 };
